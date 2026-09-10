53939676d292646369a012704aa0f538fe73490b
zwelakhe <zwelakhe@users.noreply.github.com>
2026-09-10 20:36:54 +0200

    adding staging env

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+// Staging environment
+export const environment = {
+  production: false,
+  apiUrl: 'https://backend-rentuhub-staging.up.railway.app/api',
+  defaultCurrency: 'ZAR',
+  defaultLocale: 'en-ZA',
+};
